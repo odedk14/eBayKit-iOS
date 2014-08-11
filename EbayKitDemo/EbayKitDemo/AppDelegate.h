cbd4d5f729dfe59bf933eb8814967d3f462fb905
odedk14 <odedklein@Odeds-MacBook-Pro.local>
2014-08-11 16:22:14 +0100

    first

---
+++ EbayKitDemo/EbayKitDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EbayKitDemo
+//
+//  Created by Oded Klein on 8/11/14.
+//  Copyright (c) 2014 Oded Klein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EbayKitDemo/EbayKitDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  EbayKitDemo
+//
+//  Created by Oded Klein on 8/11/14.
+//  Copyright (c) 2014 Oded Klein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
